46124b4384fe03f058dc094b911b461c564e8d96
David Roberts <david.roberts63@gmail.com>
2015-10-22 23:27:57 -0500

    Handle POST from webhook call

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var request = require("request");
 var poster = require("./postissue");
 
 var util = require("util");
+var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 var router = express.Router();
@@ -37,6 +38,34 @@ router.get("/pr-build-completion/:project/:pull/:build", function(req, res) {
 		console.log("Posting to Github PR");
 		settings.prNumber = req.params.pull;
 		settings.repoName = req.params.project;
+
+		poster.postImagesToIssue(settings, screenshots, function() {
+			res.type("json");
+			res.json({completum: "yep"});
+		});
+	});
+});
+
+router.post("/pr-build-completion", bodyParser.json(), function(req, res) {
+	console.log("POST build completion");
+	var project = req.body.reponame;
+	var build = req.body.build_num;
+	var pull = req.body.pull_request_urls[0];
+	pull = pull.substring(pull.lastIndexOf("/") + 1);
+	console.log("--for build %s of pull %s in the %s project", build, pull, project);
+
+	var url = "https://circleci.com/api/v1/project/" + settings.circleCiAccount + "/" + project + "/" + build + "/artifacts?circle-token=" + settings.circleCiToken;
+
+	// DRY you say? Yeah I know SOLID. Right now just learning nodejs, so it's https://www.youtube.com/watch?v=s8MDNFaGfT4
+	request({url: url, headers: { "Accept": "application/json"} }, function(err, response, body) {
+		console.log("Retrieved the artifacts payload");
+		var payload = JSON.parse(body);
+		var screenshots = payload.map(theUrl).filter(forImages);
+		console.log(screenshots);
+
+		console.log("Posting to Github PR");
+		settings.prNumber = pull;
+		settings.repoName = project;
 
 		poster.postImagesToIssue(settings, screenshots, function() {
 			res.type("json");
