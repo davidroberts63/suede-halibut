0da3584bc968deffeddd51bb6db9270771f78157
David Roberts <david.roberts63@gmail.com>
2015-11-19 23:13:19 -0600

    Refactor route handling to individual functions

--- app.js
+++ app.js
@@ -22,38 +22,63 @@ router.get("/", function(req, res) {
 	res.send("Ready when you are");
 });
 
-router.post("/pr-build-completion", bodyParser.json(), function(req, res) {
-	console.log("POST build completion");
+function buildParameters(req, res, next) {
 	var project = req.body.payload.reponame;
 	var build = req.body.payload.build_num;
 	var pull = req.body.payload.pull_request_urls[0];
 	pull = pull.substring(pull.lastIndexOf("/") + 1);
-	console.log("--for build %s of pull %s in the %s project", build, pull, project);
 
 	var url = "https://circleci.com/api/v1/project/" + settings.circleCiAccount + "/" + project + "/" + build + "/artifacts?circle-token=" + settings.circleCiToken;
 
-	request({url: url, headers: { "Accept": "application/json"} }, function(err, response, body) {
+	req.artifactsUrl = url;
+	req.pullrequestNumber = pull;
+	req.project = project;
+
+	console.log("Build %s of pull request %s in the %s project", build, pull, project);
+	console.log("Artifacts URL: %s", url.split("?").splice(0,1));
+
+	next();
+}
+
+function artifactImages(req, res, next) {
+	request({url: req.artifactsUrl, headers: { "Accept": "application/json"} }, function(err, response, body) {
 		console.log("Retrieved the artifacts payload");
 		var payload = JSON.parse(body);
 		var screenshots = payload.map(theUrl).filter(forImages);
 		console.log(screenshots);
 
-		console.log("Posting to Github PR");
-		settings.prNumber = pull;
-		settings.repoName = project;
-
-		poster.postImagesToIssue(settings, screenshots, function() {
-			res.type("json");
-			res.json({completum: "yep"});
-		});
+		next();
 	});
-});
+}
+
+function postToPullRequest(req, res, next) {
+	settings.prNumber = req.pullrequestNumber;
+	settings.repoName = req.project;
+	console.log("Posting for PR # %s of %s", settings.prNumber, settings.repoName);
+
+	poster.postImagesToIssue(settings, screenshots, function() {
+		res.type("json");
+		res.json({completum: "yep"});
+	});
+
+	next();
+}
+
+function respondWithMaybe(req, res) {
+	res.json({completum: "maybe"});
+}
+
+router.post("/pr-build-completion",
+	bodyParser.json(),
+	buildParameters,
+	artifactImages,
+	respondWithMaybe);
 
 app.use("/", router);
 
 var port = process.env["PORT"] || 8080;
 
-console.log("Preparing to listen on %s:%d", ipaddress, port);
+console.log("Preparing to listen on port %d", port);
 var server = app.listen(port, function() {
 	console.log('%s: Node server started on port %d ...',
 							Date(Date.now() ), port);
